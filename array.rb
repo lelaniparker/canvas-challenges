765d5ff5fbcee577d858487f2978c5e998d11394
lelani82 <lelaniparker@hotmail.com>
2019-04-02 15:52:30 +1000

    first commit

---
+++ array.rb
@@ -0,0 +1,11 @@
+four_letter_animals = ["Calf", "Duck", "Elephant", "Goat", "Lamb", "Lion", "Mule", "Dog"]
+
+four_letter_animals.push("Puma")
+four_letter_animals.insert(4, "Joey")
+four_letter_animals.delete("Dog")
+four_letter_animals.reverse!
+four_letter_animals[-3] = "Foal"
+four_letter_animals << "Bear"
+four_letter_animals.reverse!
+
+puts four_letter_animals
