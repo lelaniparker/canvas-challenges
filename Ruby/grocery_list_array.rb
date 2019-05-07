218bda0c023ab03d733bfe4921a2130f6e8daca9
lelani82 <lelaniparker@hotmail.com>
2019-05-07 18:47:35 +1000

    completed arrays beast mode

--- Ruby/grocery_list_array.rb
+++ Ruby/grocery_list_array.rb
@@ -4,26 +4,23 @@ grocery_list = []
 # Loop
 3.times do
 puts "What item do you want to add to your grocery list?"
-grocery_item = gets.chomp
+grocery_item = gets.downcase.chomp
+grocery_list << grocery_item
 end
 
 # Replace Ice Cream with Broccoli
-# if (grocery_item == "ice cream")
-#     grocery_list << "Broccoli"
-# else
-#     grocery_list << grocery_item
-# end 
+grocery_list = grocery_list.map do |item|
+    if item == "ice cream"
+        "broccoli"
+    else
+        item
+    end
+end 
 
-puts grocery_list
-#ice_cream = gets.downcase
+# Length of grocery list
+grocery_list_length = grocery_list.length
+puts "You have  #{grocery_list_length} items in your grocery list"
 
-# # Sort grocery List
-# grocery_list_sorted = grocery_list.sort!
-# puts "Here\'s your sorted grocery list: #{grocery_list_sorted}"
-
-# # Length of grocery list
-# grocery_list_length = grocery_list.length
-# puts "You have  #{grocery_list_length} items in your grocery list"
-
-
-
+# Sort grocery List
+grocery_list_sorted = grocery_list.sort!
+puts "Here\'s your sorted grocery list: #{grocery_list_sorted}"
